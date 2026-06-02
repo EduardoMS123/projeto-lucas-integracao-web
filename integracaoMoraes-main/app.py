f32307bbf38631fa3fd9da9d97e4c0b884a83768
EduardoMS <eduardo.montanha.santos@escola.pr.gov.br>
2026-06-02 08:12:27 -0300

    Add files via upload

---
+++ integracaoMoraes-main/app.py
@@ -0,0 +1,13 @@
+#import do framework flask
+#import do render_template para leitura html
+#request para a captura dos dados
+from flask import Flask, render_template, request
+#biblioteca para criar a conexão com mysql
+import mysql.connector as sqlconec
+
+
+
+
+
+
+
